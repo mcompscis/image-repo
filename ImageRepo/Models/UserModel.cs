97df0f1cd45256f58d0115e331d87572125f0859
Mayank Shrivastava <mayank@radcommsystems.com>
2021-09-21 20:33:04 -0400

    initial

---
+++ ImageRepo/Models/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageRepo.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public int UserName { get; set; }
+        public int Email { get; set; }
+    }
+}
